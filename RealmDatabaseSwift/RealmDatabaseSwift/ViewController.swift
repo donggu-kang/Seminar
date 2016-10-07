fb357e4ce674f3b2d8cae7ee96452dcdd206c226
tylenol <jungsanmango@gmail.com>
2016-10-08 01:58:00 +0900

    Added Swift Files

---
+++ RealmDatabaseSwift/RealmDatabaseSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmDatabaseSwift
+//
+//  Created by tylenol on 2016. 10. 8..
+//  Copyright © 2016년 tylenolG00py. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
