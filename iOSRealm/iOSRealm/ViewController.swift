ac7d1fc390f050ad82117b13344b80d9d12f6714
tylenol <jungsanmango@gmail.com>
2016-10-08 14:07:22 +0900

    Completed Swift Project

--- iOSRealm/iOSRealm/ViewController.swift
+++ iOSRealm/iOSRealm/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Cat : Object {
+    dynamic var name = ""
+    dynamic var detail = ""
+}
 
 class ViewController: UIViewController {
 
+    let realm = try! Realm()
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var detailField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var searchField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    @IBAction func search(_ sender: AnyObject) {
+        
+        resultLabel.text = realm.objects(Cat.self).filter("name contains '" + searchField.text! + "'").first?.detail
+        
+        
     }
 
+    @IBAction func add(_ sender: AnyObject) {
+        
+        
+        let cat = Cat()
+        
+        cat.name = nameField.text!
+        cat.detail = detailField.text!
+        
+        try! realm.write {
+            realm.add(cat)
+        }
+        
+    }
 
 }
 
